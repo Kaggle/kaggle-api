1ad0b272c2f57c4a3cc4a91957458d8344f18fa4
Peretz Cohen <pizzaz93@users.noreply.github.com>
2021-03-12 16:30:00 -0800

    Update kaggle_api_extended.py
    update version number

--- kaggle/api/kaggle_api_extended.py
+++ kaggle/api/kaggle_api_extended.py
@@ -82,7 +82,7 @@ except NameError:
 
 
 class KaggleApi(KaggleApi):
-    __version__ = '1.5.11'
+    __version__ = '1.5.12'
 
     CONFIG_NAME_PROXY = 'proxy'
     CONFIG_NAME_COMPETITION = 'competition'

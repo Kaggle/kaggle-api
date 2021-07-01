cff454c3133bb6367edb77219ac516b74326ef03
fabian40 <fawolfmann@gmail.com>
2021-07-01 01:23:18 -0300

    prevent to have the creds on import time

--- kaggle/__init__.py
+++ kaggle/__init__.py
@@ -19,5 +19,3 @@ from __future__ import absolute_import
 from kaggle.api.kaggle_api_extended import KaggleApi
 from kaggle.api_client import ApiClient
 
-api = KaggleApi(ApiClient())
-api.authenticate()

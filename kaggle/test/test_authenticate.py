a4fa960a27df2fa4b33abf7d3ec2263f0d0e46aa
Steve Messick <messick@google.com>
2024-09-18 16:05:14 +0000

    Format with two space indent

--- kaggle/test/test_authenticate.py
+++ kaggle/test/test_authenticate.py
@@ -24,36 +24,36 @@ import unittest
 
 class TestAuthenticate(unittest.TestCase):
 
-    def setUp(self):
-        print("setup             class:%s" % self)
+  def setUp(self):
+    print("setup             class:%s" % self)
 
-    def tearDown(self):
-        print("teardown          class:TestStuff")
+  def tearDown(self):
+    print("teardown          class:TestStuff")
 
-    # Environment
+  # Environment
 
-    def test_environment_variables(self):
-        os.environ['KAGGLE_USERNAME'] = 'dinosaur'
-        os.environ['KAGGLE_KEY'] = 'xxxxxxxxxxxx'
-        api = KaggleApi()
+  def test_environment_variables(self):
+    os.environ['KAGGLE_USERNAME'] = 'dinosaur'
+    os.environ['KAGGLE_KEY'] = 'xxxxxxxxxxxx'
+    api = KaggleApi()
 
-        # We haven't authenticated yet
-        self.assertTrue("key" not in api.config_values)
-        self.assertTrue("username" not in api.config_values)
-        api.authenticate()
+    # We haven't authenticated yet
+    self.assertTrue("key" not in api.config_values)
+    self.assertTrue("username" not in api.config_values)
+    api.authenticate()
 
-        # Should be set from the environment
-        self.assertEqual(api.config_values['key'], 'xxxxxxxxxxxx')
-        self.assertEqual(api.config_values['username'], 'dinosaur')
+    # Should be set from the environment
+    self.assertEqual(api.config_values['key'], 'xxxxxxxxxxxx')
+    self.assertEqual(api.config_values['username'], 'dinosaur')
 
-    # Configuration Actions
+  # Configuration Actions
 
-    def test_config_actions(self):
-        api = KaggleApi()
+  def test_config_actions(self):
+    api = KaggleApi()
 
-        self.assertTrue(api.config_dir.endswith('kaggle'))
-        self.assertEqual(api.get_config_value('doesntexist'), None)
+    self.assertTrue(api.config_dir.endswith('kaggle'))
+    self.assertEqual(api.get_config_value('doesntexist'), None)
 
 
 if __name__ == '__main__':
-    unittest.main()
+  unittest.main()

b896b817b60d9013a0fe69f5c2298e399be0569f
stevemessick <stevemessick@users.noreply.github.com>
2025-01-30 15:12:51 -0800

    Update copyrights (#685)
    This is all done automatically.

--- kaggle/models/start_blob_upload_response.py
+++ kaggle/models/start_blob_upload_response.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python
 #
-# Copyright 2024 Kaggle Inc
+# Copyright 2025 Kaggle Inc
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.

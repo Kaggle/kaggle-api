c9ec198a3a2193633351f664b8ee228734387fcb
Jim Plotts <jplotts@google.com>
2025-05-19 14:20:31 -0400

    Output docker_image as part of the pull metadata (#773)
    https://screenshot.googleplex.com/8GwAdfVrSwADp5W
    
    http://b/417413172

--- src/kaggle/api/kaggle_api_extended.py
+++ src/kaggle/api/kaggle_api_extended.py
@@ -2816,6 +2816,7 @@ class KaggleApi:
             data['kernel_sources'] = server_metadata.kernel_data_sources
             data['competition_sources'] = server_metadata.competition_data_sources
             data['model_sources'] = server_metadata.model_data_sources
+            data['docker_image'] = server_metadata.docker_image
             with open(metadata_path, 'w') as f:
                 json.dump(data, f, indent=2)
 

6d8158d6839ed8a23e691f0e95e7254a4b38d890
Steve Messick <messick@google.com>
2025-11-24 20:06:26 +0000

    Fix build script

--- src/kaggle/__init__.py
+++ src/kaggle/__init__.py
@@ -3,7 +3,7 @@ from __future__ import absolute_import
 import os
 from kaggle.api.kaggle_api_extended import KaggleApi
 
-__version__ = "1.8.0"
+__version__ = "1.8.2"
 
 enable_oauth = os.environ.get("KAGGLE_ENABLE_OAUTH") in ("1", "true", "yes")
 api = KaggleApi(enable_oauth=enable_oauth)

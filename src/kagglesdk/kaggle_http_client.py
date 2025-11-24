d4e64eac5067248eeaccf1be37de83ab5ccede3e
Steve Messick <messick@google.com>
2025-11-24 18:31:29 +0000

    format

--- src/kagglesdk/kaggle_http_client.py
+++ src/kagglesdk/kaggle_http_client.py
@@ -83,12 +83,12 @@ class KaggleHttpClient(object):
 
         # Merge environment settings into session
         settings = self._session.merge_environment_settings(http_request.url, {}, None, None, None)
-        
+
         # Use stream=True for file downloads to avoid loading entire file into memory
         # See: https://github.com/Kaggle/kaggle-api/issues/754
         if response_type is not None and (response_type == FileDownload or response_type == HttpRedirect):
-            settings['stream'] = True
-        
+            settings["stream"] = True
+
         http_response = self._session.send(http_request, **settings)
 
         response = self._prepare_response(response_type, http_response)
